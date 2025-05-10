20dd2c8f0285214f2fe7d3da11c42aec4cfe31d5
PieroMeza10 <pieroms0905@gmail.com>
2025-05-10 15:26:09 -0500

    QA1

---
+++ src/ejercicio2/QueueArray.java
@@ -0,0 +1,10 @@
+package ejercicio2;
+
+import Actividad2.Queue;
+import actividad1.ExceptionIsEmpty;
+
+public class QueueArray<E> implements Queue<E> {
+    private E[] array;
+    private int first; 
+
+}

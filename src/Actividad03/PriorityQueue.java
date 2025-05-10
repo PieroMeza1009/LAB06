f3b3e0f2d674fc5455a18df6773889c4969c7cd8
PieroMeza10 <pieroms0905@gmail.com>
2025-05-10 14:45:41 -0500

    PQ4

--- src/Actividad03/PriorityQueue.java
+++ src/Actividad03/PriorityQueue.java
@@ -2,13 +2,12 @@ package Actividad03;
 
 import actividad1.ExceptionIsEmpty;
 
-public interface PriorityQueue {
 public interface PriorityQueue<E, N> {
     void enqueue(E x, N pr);
         E dequeue() throws ExceptionIsEmpty;
     E front() throws ExceptionIsEmpty;
     E back() throws ExceptionIsEmpty;
     boolean isEmpty();
-}
+
 
 }

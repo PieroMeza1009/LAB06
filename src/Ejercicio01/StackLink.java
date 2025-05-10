f30eea02539278a5749c70f6ef73691de4cce831
PieroMeza10 <pieroms0905@gmail.com>
2025-05-10 15:13:52 -0500

    st2

--- src/Ejercicio01/StackLink.java
+++ src/Ejercicio01/StackLink.java
@@ -3,6 +3,7 @@ package Ejercicio01;
 import actividad1.Stack;
 import actividad1.ExceptionIsEmpty;
 
-public class StackLink {
+public class StackLink <E> implements Stack<E> {
+    private Node<E> top;
 
 }

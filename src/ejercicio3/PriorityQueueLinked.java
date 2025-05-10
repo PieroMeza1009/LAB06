6cc8371b075bc0eaaea3d30c94a20ecb382b1822
PieroMeza10 <pieroms0905@gmail.com>
2025-05-10 16:13:40 -0500

    pqll2

--- src/ejercicio3/PriorityQueueLinked.java
+++ src/ejercicio3/PriorityQueueLinked.java
@@ -5,6 +5,11 @@ import Actividad2.QueueLink;
 import Actividad2.Queue;
 import actividad1.ExceptionIsEmpty;
 
-public class PriorityQueueLinked {
+public class PriorityQueueLinked <E> implements PriorityQueue<E, Integer> {
+
+    private Queue<E>[] colas;
+    private int prioridades;
+
+    public PriorityQueueLinked(int prioridades) {
 
 }

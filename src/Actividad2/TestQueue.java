5ccf3b85fb40c3e43859374b698b1d6b93487a92
PieroMeza10 <pieroms0905@gmail.com>
2025-05-10 14:33:43 -0500

    TQ3

--- src/Actividad2/TestQueue.java
+++ src/Actividad2/TestQueue.java
@@ -4,4 +4,8 @@ public class TestQueue {
     public static void main(String[] args) {
         Queue<String> cola = new QueueLink<>();
 
+        cola.enqueue("A");
+        cola.enqueue("B");
+        cola.enqueue("C");
+
 }

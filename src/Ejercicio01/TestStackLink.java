aa9b49a3faa5421c37dd47592cbb4c8eb21f0165
PieroMeza10 <pieroms0905@gmail.com>
2025-05-10 15:19:47 -0500

    testsl3

--- src/Ejercicio01/TestStackLink.java
+++ src/Ejercicio01/TestStackLink.java
@@ -12,9 +12,18 @@ public class TestStackLink {
         pila.push("Uno");
         pila.push("Dos");
         pila.push("Tres");
-    }
+    
 
         System.out.println("Contenido de la pila:");
         System.out.println(pila);
+
+        try {
+            System.out.println("Tope: " + pila.top());
+            System.out.println("Desapilado: " + pila.pop());
+            System.out.println("Nuevo tope: " + pila.top());
+        } catch (ExceptionIsEmpty e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
 }

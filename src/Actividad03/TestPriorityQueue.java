5ba7cef89d84bba13c3a4ea4b06ad7a8cd2a43bb
PieroMeza10 <pieroms0905@gmail.com>
2025-05-10 15:04:19 -0500

    tpq2

--- src/Actividad03/TestPriorityQueue.java
+++ src/Actividad03/TestPriorityQueue.java
@@ -5,5 +5,8 @@ import Actividad03.PriorityQueueLinkSort;
 import actividad1.ExceptionIsEmpty;
 
 public class TestPriorityQueue {
+    public static void main(String[] args) {
+        PriorityQueue<String, Integer> cola = new PriorityQueueLinkSort<>();
+    }
 
 }

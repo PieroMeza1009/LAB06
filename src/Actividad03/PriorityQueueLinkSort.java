4320998c69a5fdc7ed97866f9b1c76179637442c
PieroMeza10 <pieroms0905@gmail.com>
2025-05-10 14:55:51 -0500

    pql3

--- src/Actividad03/PriorityQueueLinkSort.java
+++ src/Actividad03/PriorityQueueLinkSort.java
@@ -1,10 +1,12 @@
 package Actividad03;
 import actividad1.ExceptionIsEmpty;
 
-public class PriorityQueueLinkSort {
+public class PriorityQueueLinkSort<E, N extends Comparable<N>> implements PriorityQueue<E, N> {
 
     class EntryNode {
         E data;
         N priority;
+    
+    }
 
 }

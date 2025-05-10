7f6f616bb2663d529f40dc3b2289c1baa252e937
PieroMeza10 <pieroms0905@gmail.com>
2025-05-10 15:04:32 -0500

    tpq3

--- src/Actividad03/TestPriorityQueue.java
+++ src/Actividad03/TestPriorityQueue.java
@@ -7,6 +7,15 @@ import actividad1.ExceptionIsEmpty;
 public class TestPriorityQueue {
     public static void main(String[] args) {
         PriorityQueue<String, Integer> cola = new PriorityQueueLinkSort<>();
+
+        cola.enqueue("Tarea A", 2);
+        cola.enqueue("Tarea B", 5);
+        cola.enqueue("Tarea C", 3);
+        cola.enqueue("Tarea D", 5);
+        cola.enqueue("Tarea E", 1);
+
+        System.out.println("Contenido de la cola de prioridad:");
+        System.out.println(cola);
     }
 
 }

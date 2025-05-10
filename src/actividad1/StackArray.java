c3b69f31b6c652655ced262ead66452d4affa173
PieroMeza10 <pieroms0905@gmail.com>
2025-05-10 12:49:01 -0500

    SA2

--- src/actividad1/StackArray.java
+++ src/actividad1/StackArray.java
@@ -4,4 +4,8 @@ public class StackArray  implements Stack<E> {
     private E[] array;
     private int tope;
 
+        public StackArray(int n) {
+        this.array = (E[]) new Object[n];
+        this.tope = -1;
+    }
 }

a9710c8ebcf18c22a745622d20eb57421618af39
PieroMeza10 <pieroms0905@gmail.com>
2025-05-10 16:17:49 -0500

    ttt3

--- src/ejercicio3/TestPriorityQueueLinked.java
+++ src/ejercicio3/TestPriorityQueueLinked.java
@@ -5,5 +5,12 @@ import Actividad03.PriorityQueue;
 import actividad1.ExceptionIsEmpty;
 
 public class TestPriorityQueueLinked {
+    public static void main(String[] args) {
+        PriorityQueue<String, Integer> pq = new PriorityQueueLinked<>(5);
+        pq.enqueue("Tarea urgente", 4);
+        pq.enqueue("Tarea normal", 2);
+        pq.enqueue("Tarea baja", 1);
+        pq.enqueue("Tarea crítica", 4);
+        pq.enqueue("Tarea sin apuro", 0);
 
 }

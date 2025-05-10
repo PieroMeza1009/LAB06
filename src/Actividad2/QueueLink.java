55516ed3bfd7711c66be51638d910fbd6e975776
PieroMeza10 <pieroms0905@gmail.com>
2025-05-10 13:33:14 -0500

    QL3

--- src/Actividad2/QueueLink.java
+++ src/Actividad2/QueueLink.java
@@ -4,4 +4,9 @@ public class QueueLink<E> implements Queue<E> {
     private Node<E> first;
     private Node<E> last;
 
+    public QueueLink() {
+        this.first = null;
+        this.last = null;
+    }
+
 }

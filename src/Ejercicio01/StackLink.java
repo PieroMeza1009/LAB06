ba88074f44f32b043c25da38367dd2f6bd7199c7
PieroMeza10 <pieroms0905@gmail.com>
2025-05-20 16:28:44 -0500

    ccc1

--- src/Ejercicio01/StackLink.java
+++ src/Ejercicio01/StackLink.java
@@ -3,22 +3,26 @@ package Ejercicio01;
 import actividad1.Stack;
 import actividad1.ExceptionIsEmpty;
 
+//implementa la interfaz Stack, usando una lista enlazada
 public class StackLink<E> implements Stack<E> {
-    private Node<E> top;
+    private Node<E> top; //nodo que representa el tope (el último elemento ingresado) de la pila
+    // al inicio, la pila está vacía (top es null)
     public StackLink() {
         this.top = null;
     }
 
+    //// metodo para agregar un elemento a la pila (push)
     public void push(E x) {
-        Node<E> nuevo = new Node<>(x);
+        Node<E> nuevo = new Node<>(x); ///// Se crea un nuevo nodo con el valor x
         nuevo.setNext(top);
-        top = nuevo;
+        top = nuevo; //ahora el tope va a ser el nuevo nodo
     }
 
+    //este es un metodo para eliminar y devolver el elemento del tope (pop)
     public E pop() throws ExceptionIsEmpty {
         if (isEmpty()) throw new ExceptionIsEmpty("Stack is empty");
         E valor = top.getData();
-        top = top.getNext();
+        top = top.getNext(); //// El nuevo tope pasa a ser el siguiente nodo
         return valor;
     }
 
